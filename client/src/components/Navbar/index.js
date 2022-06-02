979fbd781af222b97bfeb0136930adbb6aa15d89
Unni Ramachandran <unnipbvr@gmail.com>
2022-06-02 19:56:53 -0400

    Coverted the app to "Single Page App"

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
-export default class index extends Component {
+export class index extends Component {
   render() {
     return (
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand href="/">Nalanda Latex Management System!!!</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/">Customers</Nav.Link>
+          {/* <Nav.Link href="/">Customers</Nav.Link>
           <Nav.Link href="/latexCollection">Latex Collection</Nav.Link>
           <Nav.Link href="#cashPayments">Cash Payments</Nav.Link>
-          <Nav.Link href="#billingInvoices">Billing & Invoices</Nav.Link>
+          <Nav.Link href="#billingInvoices">Billing & Invoices</Nav.Link> */}
+          <Link className="mr-3" style={{ color: "black" }} to="/">
+            Customers
+          </Link>
+          <Link className="mr-3" style={{ color: "black" }} to="/latexCollection">
+            Latex Collection
+          </Link>
+          <Link className="mr-3" style={{ color: "black" }} to="/cashPayments">
+            Cash Payments
+          </Link>
+          <Link className="mr-3" style={{ color: "black" }} to="/billingInvoices">
+            Billing & Invoices
+          </Link>
         </Nav>
       </Navbar>
     );
   }
 }
+export default withRouter(index);
