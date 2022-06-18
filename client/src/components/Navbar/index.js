cf5c40aeeb94ed7bfca96abf9aed5623aa383fb9
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2022-06-18 23:33:11 +0530

    Nav bar styled

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { withRouter, Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
+import "./style.css";
 export class index extends Component {
   render() {
     return (
       <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/">Nalanda Associates</Navbar.Brand>
+        <Navbar.Brand id="title" href="/">Nalanda Associates</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/">Customers</Nav.Link>
-          <Nav.Link href="/latexCollection">Latex Collection</Nav.Link>
-          <Nav.Link href="/cashpayment">Cash Payments</Nav.Link>
-          <Nav.Link href="/billingInvoices">Billing & Invoices</Nav.Link>
+          <Nav.Link id="customer" href="/">Customers</Nav.Link>
+          <Nav.Link id ="latex" href="/latexCollection">Latex Collection</Nav.Link>
+          <Nav.Link id ="cash" href="/cashpayment">Cash Payments</Nav.Link>
+          <Nav.Link id="bill" href="/billingInvoices">Billing & Invoices</Nav.Link>
         </Nav>
       </Navbar>
     );
