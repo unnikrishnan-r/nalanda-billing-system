2e029bfde6142513b6964997d3f1bb2a556eac3a
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2022-06-19 23:42:39 +0530

    paid and unpaid symbol added

--- client/src/components/StatusRenderer.jsx
+++ client/src/components/StatusRenderer.jsx
@@ -1,11 +1,22 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import "./style.css";
 export default class StatusRenderer extends Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <label>{this.props.data.paymentStatus ? "Paid" : "Unpaid"}</label>
-      )
-    }
+  constructor(props) {
+    super(props);
   }
+  render() {
+    return (
+      <div>
+        {
+          (this.props.data.paymentStatus)
+            ? <label>
+              <span className="dotPaid"></span><label id="status">Paid</label>
+              </label>
+            : <label>
+              <span className="dotUnPaid"></span><label id="status">Unpaid</label>
+              </label>
+        }
+      </div>
+    )
+  }
+}
